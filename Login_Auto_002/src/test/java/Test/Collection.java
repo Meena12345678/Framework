3f3f19c920e585fb075508f99550adcc32411b6b
MS Jain <jain.sonali2908@gmail.com>
2020-12-13 19:56:19 +0530

    TestAutomation

---
+++ Login_Auto_002/src/test/java/Test/Collection.java
@@ -0,0 +1,17 @@
+package Test;
+import java.util.*;
+
+public class Collection {
+public static void main(String[] args) {
+	Map<String,String> m1=new TreeMap<String,String>();
+	m1.put("G","Ayush1");
+	m1.put("F","Ayush2");
+	m1.put("C","Ayush3");
+	m1.put("D","Ayush4");
+	m1.put("E","Ayush5");
+	//m1.put("E9","Ayush5");
+	m1.put("B","Ayush5");
+	m1.put("A","Ayush6");
+	System.out.println(m1);
+}
+}
---
+++ Login_Auto_002/src/test/java/Test/Login.java
@@ -0,0 +1,16 @@
+package Test;
+
+import org.testng.annotations.Test;
+
+public class Login {
+@Test
+public void loginw()
+{
+	System.out.println("login");
+}
+@Test
+public void logout()
+{
+	System.out.println("logout");
+}
+}
