4b31ea081ef2888ba7fc257488196f3e58802668
Ayush Jain <jain.ayush0210@gmail.com>
2024-01-21 19:01:10 +0530

    going to log off the day

--- Login_Auto_002/src/test/java/Test/Collection.java
+++ Login_Auto_002/src/test/java/Test/Collection.java
@@ -2,10 +2,11 @@ package Test;
 import java.util.*;
 
 public class Collection {
-public static void main(String[] args) {
+public static void main1(String[] args) {
 	Map<String,String> m1=new TreeMap<String,String>();
-	m1.put("G","Ayush1");
-	m1.put("S","SonaliJain");
+	m1.put("G1","Ayush1");
+	
+	
 
 }
 }
