a5d2ebf165e3015881580313a2f6c8dce78a9946
Ayush Jain <jain.ayush0210@gmail.com>
2024-01-21 18:39:50 +0530

    dynmic name

--- Login_Auto_002/src/test/java/Test/Collection.java
+++ Login_Auto_002/src/test/java/Test/Collection.java
@@ -4,8 +4,9 @@ import java.util.*;
 public class Collection {
 public static void main(String[] args) {
 	Map<String,String> m1=new TreeMap<String,String>();
-	m1.put("G","Ayush1");
-	m1.put("S","SonaliJain");
+	m1.put("A","Name will be dynamic1");
+	m1.put("S1","Name will be dynamic2");
+	m1.put("S2","Name will be dynamic3");
 
 }
 }
