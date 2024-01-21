de6ebf19e5042f3feaf5eb6f38db24edf0b7beae
Ayush Jain <jain.ayush0210@gmail.com>
2024-01-21 18:25:07 +0530

    Changed sonali jain's name

--- Login_Auto_002/src/test/java/Test/Collection.java
+++ Login_Auto_002/src/test/java/Test/Collection.java
@@ -5,7 +5,7 @@ public class Collection {
 public static void main(String[] args) {
 	Map<String,String> m1=new TreeMap<String,String>();
 	m1.put("G","Ayush1");
-	m1.put("S","Sonali1");
+	m1.put("S","SonaliJain");
 
 }
 }
